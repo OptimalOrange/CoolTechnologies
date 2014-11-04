b73c72a035651469d0042ef525ff6eb06bdfc3fa
Bai-Jie <BaiJie1991@gmail.com>
2014-11-04 20:23:24 +0800

    添加com.orange.cooltechnologies.ui包

--- app/src/main/java/com/orange/cooltechnologies/ui/MainActivity.java
+++ app/src/main/java/com/orange/cooltechnologies/ui/MainActivity.java
@@ -1,19 +1,17 @@
-package com.orange.cooltechnologies;
+package com.orange.cooltechnologies.ui;
 
-import android.app.Activity;
+import com.orange.cooltechnologies.R;
+
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends Activity implements ActionBar.OnNavigationListener {
