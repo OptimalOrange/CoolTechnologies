51ac7b941a4e681d3dfc8b9db773b552d88d8720
Zhou-Peican <zpc20106200@gmail.com>
2014-11-30 13:47:02 +0800

    修改热门视频的布局 图片使用正常的imageview，gridview中的item使用cardview布局

--- app/src/main/java/com/optimalorange/cooltechnologies/ui/fragment/ListVideosFragment.java
+++ app/src/main/java/com/optimalorange/cooltechnologies/ui/fragment/ListVideosFragment.java
@@ -4,7 +4,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.etsy.android.grid.StaggeredGridView;
-import com.etsy.android.grid.util.DynamicHeightImageView;
 import com.optimalorange.cooltechnologies.R;
 import com.optimalorange.cooltechnologies.entity.Video;
 import com.optimalorange.cooltechnologies.util.Utils;
@@ -13,25 +12,21 @@ import com.optimalorange.cooltechnologies.util.VolleySingleton;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by WANGZHENGZE on 2014/11/20.
  * 热门
  */
 public class ListVideosFragment extends Fragment {
-
-    private static final String YOUKU_API_VIDEOS_BY_CATEGORY
-            = "https://openapi.youku.com/v2/videos/by_category.json";
 
     private static final String CATEGORY_LABEL_OF_TECH = "科技";
 
@@ -105,18 +100,12 @@ public class ListVideosFragment extends Fragment {
      */
     public class ItemsAdapter extends BaseAdapter {
 
-        private final Random mRandom;
-
         private LinkedList<Video> mVideos;
 
         private ImageLoader mImageLoader;
 
-        //记录每个图片的高度，避免滑动之后从新计算
-        private SparseArray<Double> sPositionHeightRatios = new SparseArray<Double>();
-
         public ItemsAdapter(LinkedList<Video> mVideos, ImageLoader mImageLoader) {
             super();
-            mRandom = new Random();
             this.mVideos = mVideos;
             this.mImageLoader = mImageLoader;
         }
@@ -143,7 +132,7 @@ public class ListVideosFragment extends Fragment {
                 convertView = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.list_item_videos, parent, false);
                 vh = new ViewHolder();
-                vh.thumbnail = (DynamicHeightImageView) convertView.findViewById(R.id.thumbnail);
+                vh.thumbnail = (ImageView) convertView.findViewById(R.id.thumbnail);
                 vh.duration = (TextView) convertView.findViewById(R.id.duration);
                 vh.title = (TextView) convertView.findViewById(R.id.title);
                 vh.viewCount = (TextView) convertView.findViewById(R.id.view_count);
@@ -152,10 +141,6 @@ public class ListVideosFragment extends Fragment {
                 vh = (ViewHolder) convertView.getTag();
             }
 
-            // 获取高度值
-            double mPositionHeight = getPositionRatio(position);
-            // 设置图片高度参数值
-            vh.thumbnail.setHeightRatio(mPositionHeight);
             //加载图片
             mImageLoader.get(mVideos.get(position).getThumbnail_v2(),
                     ImageLoader.getImageListener(vh.thumbnail,
@@ -165,34 +150,15 @@ public class ListVideosFragment extends Fragment {
             //显示视频标题
             vh.title.setText(mVideos.get(position).getTitle());
             //显示播放次数（这里使用字符串资源格式化）
-            vh.viewCount.setText(String.format(getString(R.string.view_count),
-                    String.valueOf(mVideos.get(position).getView_count())));
+            vh.viewCount.setText(Utils.formatViewCount(mVideos.get(position).getView_count(),
+                    parent.getContext()));
 
             return convertView;
         }
 
-        /**
-         * 获取保存的高度
-         */
-        private double getPositionRatio(final int position) {
-            double ratio = sPositionHeightRatios.get(position, 0.0);
-            if (ratio == 0) {
-                ratio = getRandomHeightRatio();
-                sPositionHeightRatios.append(position, ratio);
-            }
-            return ratio;
-        }
-
-        /**
-         * 用于随机方块的高度参数
-         */
-        private double getRandomHeightRatio() {
-            return mRandom.nextDouble() / 2.0 + 1.0;
-        }
-
         private class ViewHolder {
 
-            DynamicHeightImageView thumbnail;
+            ImageView thumbnail;
 
             TextView duration;
 
