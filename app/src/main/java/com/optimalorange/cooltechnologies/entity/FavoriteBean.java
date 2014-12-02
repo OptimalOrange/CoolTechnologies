32850864a1beb97bc2641f2ce89e41af73311a2c
wzhzai <wzhz3000@163.com>
2014-12-02 20:42:07 +0800

    收藏增加功能：下拉刷新，点击加载更多，删除收藏。

--- app/src/main/java/com/optimalorange/cooltechnologies/entity/FavoriteBean.java
+++ app/src/main/java/com/optimalorange/cooltechnologies/entity/FavoriteBean.java
@@ -11,12 +11,14 @@ public class FavoriteBean {
     public String imageUrl = "";
     public String duration = "";
     public String link = "";
+    public String videoId = "";
 
     public FavoriteBean(JSONObject jsonObject) throws JSONException {
         title = jsonObject.getString("title");
         link = jsonObject.getString("link");
         imageUrl = jsonObject.getString("thumbnail");
         duration = jsonObject.getString("duration");
+        videoId = jsonObject.getString("id");
     }
 
 }
--- app/src/main/java/com/optimalorange/cooltechnologies/ui/fragment/FavoriteFragment.java
+++ app/src/main/java/com/optimalorange/cooltechnologies/ui/fragment/FavoriteFragment.java
@@ -9,26 +9,46 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.optimalorange.cooltechnologies.R;
 import com.optimalorange.cooltechnologies.entity.FavoriteBean;
+import com.optimalorange.cooltechnologies.ui.view.PullRefreshLayout;
 import com.optimalorange.cooltechnologies.util.NetworkChecker;
 import com.optimalorange.cooltechnologies.util.Utils;
 import com.optimalorange.cooltechnologies.util.VolleySingleton;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 /**
@@ -43,6 +63,48 @@ public class FavoriteFragment extends Fragment {
     private VolleySingleton mVolleySingleton;
     private TextView mTvHint;
     private boolean mIsCreated = false;
+    private PullRefreshLayout refreshLayout;
+
+    private WindowManager windowManager = null;
+    private WindowManager.LayoutParams windowParams = null;
+    private View deleteView = null;
+    private ArrayList<FavoriteBean> favoriteBeans;
+    private FavoriteAdapter adapter;
+
+    private static final String BASE_FAVORITE_DELETE_URL = "https://openapi.youku.com/v2/videos/favorite/destroy.json";
+    private static final int DELETE_FAVORITE_OK = 0;
+    private static final int DELETE_FAVORITE_ERROR = 1;
+    private int page = 1;
+    private int total = 0;
+    private int currentCount = 0;
+    private TextView tvViewMore;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case DELETE_FAVORITE_OK:
+                    favoriteBeans.remove(msg.arg1);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                            removeWindowView();
+                            Toast.makeText(getActivity(), getString(R.string.favorite_delete_success), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    break;
+                case DELETE_FAVORITE_ERROR:
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), getString(R.string.favorite_delete_fail), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    break;
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -58,9 +120,89 @@ public class FavoriteFragment extends Fragment {
         mVolleySingleton = VolleySingleton.getInstance(getActivity());
         favoriteListView = (ListView) view.findViewById(R.id.favorite_list);
         mTvHint = (TextView) v.findViewById(R.id.favorite_hint);
+        favoriteBeans = new ArrayList<>();
+        adapter = new FavoriteAdapter(getActivity(), favoriteBeans, mVolleySingleton.getImageLoader());
+        View footer = LayoutInflater.from(getActivity()).inflate(R.layout.ll_favorite_footer, null);
+        tvViewMore = (TextView) footer.findViewById(R.id.tv_more);
+        tvViewMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)v).setText(getString(R.string.favorite_view_more_loading));
+                getJsonData();
+            }
+        });
+        favoriteListView.addFooterView(footer);
+        favoriteListView.setAdapter(adapter);
         Log.e("wzz fav", "is onCreated!!!");
-        getJsonData();
+        getNewData();
         mIsCreated = true;
+        refreshLayout = (PullRefreshLayout) view.findViewById(R.id.pull_refresh_layout);
+        refreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getNewData();
+            }
+        });
+        favoriteListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                int[] locationInWindow = new int[2];
+                view.getLocationInWindow(locationInWindow);
+                windowParams = new WindowManager.LayoutParams();
+                windowParams.gravity = Gravity.TOP | Gravity.LEFT;
+                windowParams.x = locationInWindow[0] + view.getWidth() / 2;
+                windowParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                windowParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                //添加属性FLAG_WATCH_OUTSIDE_TOUCH，用于监听窗口外Touch事件
+                windowParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                windowParams.format = PixelFormat.TRANSLUCENT;
+                windowParams.windowAnimations = 0;
+                windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+                if (deleteView != null) {
+                    windowManager.removeViewImmediate(deleteView);
+                }
+                deleteView = LayoutInflater.from(getActivity()).inflate(R.layout.ll_favorite_delete, null);
+                deleteView.measure(0, 0);
+                int deleteViewHeight = deleteView.getMeasuredHeight();
+                windowParams.y = locationInWindow[1] - deleteViewHeight;
+                deleteView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sendDeleteRequest(favoriteBeans.get(position).videoId, position);
+                    }
+                });
+                windowManager.addView(deleteView, windowParams);
+                return true;
+            }
+        });
+
+        favoriteListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    removeWindowView();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void removeWindowView() {
+        if (windowManager != null && deleteView != null) {
+            windowManager.removeViewImmediate(deleteView);
+            deleteView = null;
+        }
+    }
+
+    private void getNewData() {
+        page = 1;
+        total = 0;
+        currentCount = 0;
+        favoriteBeans.clear();
+        getJsonData();
     }
 
     public void getJsonData() {
@@ -70,28 +212,37 @@ public class FavoriteFragment extends Fragment {
                 setHint(R.string.favorite_hint_no_net);
             }
             RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-            String url = FAVORITE_BASE_URL + "?client_id=" + getString(R.string.youku_client_id) + "&access_token=" + token;
+            String url = FAVORITE_BASE_URL + "?client_id=" + getString(R.string.youku_client_id) + "&access_token=" + token + "&page=" + page + "&count=10";
             Log.e("wzz json", "url=" + url);
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject jsonObject) {
                     try {
-                        ArrayList<FavoriteBean> favoriteBeans = new ArrayList<>();
-                        int total = jsonObject.getInt("total");
+                        total = jsonObject.getInt("total");
                         if (total != 0) {
                             JSONArray videoArray = jsonObject.getJSONArray("videos");
+                            int pageCount = videoArray.length();
+                            currentCount += pageCount;
+                            if (currentCount == total) {
+                                tvViewMore.setEnabled(false);
+                                tvViewMore.setText(getString(R.string.favorite_view_more_last));
+                            } else {
+                                tvViewMore.setEnabled(true);
+                                tvViewMore.setText(getString(R.string.favorite_view_more));
+                                page ++;
+                            }
                             for (int i = 0; i < videoArray.length(); i++) {
                                 JSONObject itemObject = videoArray.getJSONObject(i);
                                 FavoriteBean bean = new FavoriteBean(itemObject);
                                 favoriteBeans.add(bean);
                             }
                         }
-                        FavoriteAdapter adapter = new FavoriteAdapter(getActivity(), favoriteBeans, mVolleySingleton.getImageLoader());
-                        favoriteListView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
                         favoriteListView.setVisibility(View.VISIBLE);
                         if (mTvHint.getVisibility() == View.VISIBLE) {
                             mTvHint.setVisibility(View.GONE);
                         }
+                        refreshLayout.setRefreshing(false);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -177,9 +328,57 @@ public class FavoriteFragment extends Fragment {
                 mIsCreated = false;
             } else {
                 if (favoriteListView.getVisibility() == View.GONE) {
-                    getJsonData();
+                    getNewData();
                 }
             }
         }
+    }
+
+    private void sendDeleteRequest(String id, final int index) {
+        if (!NetworkChecker.isConnected(getActivity())) {
+            Toast.makeText(getActivity(), R.string.favorite_delete_no_net, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String token = Utils.getString(getActivity(), "user_token", "");
+        if (token.isEmpty()) {
+            Toast.makeText(getActivity(), R.string.favorite_delete_no_login, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        final HttpPost request = new HttpPost(BASE_FAVORITE_DELETE_URL);
+        ArrayList<NameValuePair> paramList = new ArrayList<NameValuePair>();
+        BasicNameValuePair param;
+        param = new BasicNameValuePair("client_id", getString(R.string.youku_client_id));
+        paramList.add(param);
+        param = new BasicNameValuePair("access_token", token);
+        paramList.add(param);
+        param = new BasicNameValuePair("video_id", id);
+        paramList.add(param);
+        try {
+            request.setEntity(new UrlEncodedFormEntity(paramList, HTTP.UTF_8));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpClient client = new DefaultHttpClient();
+                try {
+                    HttpResponse response = client.execute(request);
+                    Message msg = new Message();
+                    if (response.getStatusLine().getStatusCode() == 200) {
+                        msg.what = DELETE_FAVORITE_OK;
+                        msg.arg1 = index;
+                        msg.obj = EntityUtils.toString(response.getEntity());
+                        mHandler.handleMessage(msg);
+                    } else {
+                        msg.what = DELETE_FAVORITE_ERROR;
+                        mHandler.handleMessage(msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
