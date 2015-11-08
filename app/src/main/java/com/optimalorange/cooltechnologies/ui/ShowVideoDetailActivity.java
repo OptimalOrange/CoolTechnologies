1e7f6958ad17389b4b99d4be2a802fe085e3d213
Bai Jie <baijie1991@gmail.com>
2015-11-08 21:33:56 +0800

    为视频详情页添加title和thumbnail显示
    新增 视频详情页的视频标题和截图
    新增 使用percent支持库
    修改 视频详情页使用CollapsingToolbarLayout实现AppBar
    修改 重命名两个styles：ActionBarThemeOverlay->AppTheme.AppBarOverlay, ActionBarPopupThemeOverlay->AppTheme.PopupOverlay

--- app/src/main/java/com/optimalorange/cooltechnologies/ui/ShowVideoDetailActivity.java
+++ app/src/main/java/com/optimalorange/cooltechnologies/ui/ShowVideoDetailActivity.java
@@ -2,6 +2,7 @@ package com.optimalorange.cooltechnologies.ui;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.optimalorange.cooltechnologies.R;
 import com.optimalorange.cooltechnologies.network.NetworkChecker;
 import com.optimalorange.cooltechnologies.network.VideoDetailRequest;
@@ -15,7 +16,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -58,7 +64,13 @@ public class ShowVideoDetailActivity extends AppCompatActivity {
         }
         mVideo = video;
         if (mViews != null) {
+            mViews.title.setText(video.title);
             mViews.description.setText(video.description);
+            final ImageLoader.ImageListener imageListener = ImageLoader.getImageListener(
+                    mViews.thumbnail, 0, 0
+            );
+            VolleySingleton.getInstance(mViews.thumbnail.getContext()).getImageLoader()
+                    .get(video.bigThumbnail, imageListener);
         }
     }
 
@@ -74,10 +86,29 @@ public class ShowVideoDetailActivity extends AppCompatActivity {
         mVolleySingleton = VolleySingleton.getInstance(this);
         mYoukuClientId = getString(R.string.youku_client_id);
 
-        setContentView(R.layout.activity_show_video_detail);
-        mViews = new ViewHolder(this);
+        initViews();
 
         mVolleySingleton.addToRequestQueue(buildVideoDetailRequest(mVideoIdExtra));
+    }
+
+    private void initViews() {
+        setContentView(R.layout.activity_show_video_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mViews = new ViewHolder(this);
+
+        setTitle(null);
     }
 
     @Override
@@ -139,10 +170,16 @@ public class ShowVideoDetailActivity extends AppCompatActivity {
 
     private static class ViewHolder {
 
+        TextView title;
+
         TextView description;
 
+        ImageView thumbnail;
+
         ViewHolder(Activity container) {
+            title = (TextView) container.findViewById(R.id.title);
             description = (TextView) container.findViewById(R.id.description);
+            thumbnail = (ImageView) container.findViewById(R.id.app_bar_image);
         }
     }
 
